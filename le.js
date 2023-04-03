b1a77862bfc3458770fe981404dd471bc67917d5
retro-noob <canmol286@gmail.com>
2023-04-03 15:04:40 +0530

    initial commit

---
+++ le.js
@@ -0,0 +1,12 @@
+function f(){
+	console.log("top");
+document.querySelector("a").innerHTML=`About`;
+document.querySelector("p").innerHTML=`Work`;
+document.querySelector("span").innerHTML=`Contact`;
+
+ document.querySelector("a").style.color="red";
+document.querySelector("p").style.color="white";
+document.querySelector("span").style.color="white";
+  alert("This Website Look Amazing on Big Screens.");
+
+}
